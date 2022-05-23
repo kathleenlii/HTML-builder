232dd64b49cc8ca86cee04decbf86bbc6cee771d
Katya Litvinchuk <77808444+kathleenlii@users.noreply.github.com>
2022-05-23 22:05:49 +0200

    feat: add solution (Mon, May 23, 2022 13:21:30 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const readStream = fs.createReadStream(path.join( __dirname,'.text.txt'));
+const readStream = fs.createReadStream(path.join(__dirname,'text.txt'), 'utf-8');
 
 readStream.on('data', chunk => console.log(chunk));
