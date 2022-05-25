465d7ab84e63342d7d93cc66880a739654b0a439
Katya Litvinchuk <katyalitvinchuk.1@gmail.com>
2022-05-25 14:59:13 +0300

    feat: add solution (Mon, May 23, 2022 17:04:21 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+    });
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+        if (err) throw err;
+
+        files.forEach((file) => {
+            fs.copyFile(
+                path.join(__dirname, 'files', file),
+                path.join(__dirname, 'files-copy', file),
+                () => {
+                }
+            );
+        });
+    });
+});
